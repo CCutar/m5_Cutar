b16a974eaef95bad16cf118b2f12ee51e22f25a2
CRISTIAN ALEXANDRU CUTAR <cristiancutar@iesmontsia.org>
2022-11-28 21:23:19 +0100

    VersionPrincipa

--- Comparar2Notes.java
+++ Comparar2Notes.java
@@ -4,15 +4,22 @@ import java.util.Scanner;
 
 public class Comparar2Notes {
     public static void main(String[] args) {
-        Scanner teclat = new Scanner(System.in);
-        int nota = teclat.nextInt();
+        Scanner scan = new Scanner(System.in); // Declaro el scanner
 
-        int max = nota;
+        int max = 0; // declaramos max
+        /*
+         * Haremos que el usuario introduzaca 10 notas y que se guarden la nota maxima
+         * minima
+         */
+        for (int i = 0; i <= 4; i++) {
 
-        if (nota > max) {
-            max = nota;
+            int nota = scan.nextInt(); // Declaro una variable que sera un numero introducido por el usuario
+
+            if (nota > max) { // si el numero nota es mayor que nota actualzamos max
+                max = nota;
+            }
         }
-
+        /* Imprimimos max y min */
         System.out.println(max);
     }
 }
